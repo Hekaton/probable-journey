47b992a6aa9d2cd1cc38cf6643f69a7381648499
Dmitri Kolytchev <kolytchev.d@gmail.com>
2017-12-02 18:48:00 -0500

    Tweaking movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
-
+public class Movement : MonoBehaviour
+{
+	private Rigidbody rb;
+	private Vector3 direction;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
 	}
+
+
+	public float speed = 1f;
+	public 
 	
 	// Update is called once per frame
-	void Update ()
-    {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Translate( x,0, 0);
-        transform.Translate(0, 0, z);
-    }
+	void FixedUpdate ()
+	{
+		direction = Vector3.ClampMagnitude(Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal"), 1f) * speed;
+		direction.y = rb.velocity.y;
+		rb.velocity = direction;
+	}
 }
